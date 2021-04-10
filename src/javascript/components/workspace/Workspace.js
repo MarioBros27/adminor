3cd5be1fe02bcbd17fb69500e0a3b52332ef43d2
andres <andres@andress-MacBook-Pro.local>
2021-04-10 09:53:16 -0600

    initialization

---
+++ src/javascript/components/workspace/Workspace.js
@@ -0,0 +1,22 @@
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import React, {useState} from 'react'
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import { Typography } from '@material-ui/core';
+import React,{useState} from 'react'
+import { Dialog, DialogContent } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
+// import axios from 'axios'
+import React, { useState, useEffect, Fragment } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Column from './Column'
+import { makeStyles } from '@material-ui/core/styles';
+import FormDialog from './FormDialog'
+import Typography from '@material-ui/core/Typography';
+
