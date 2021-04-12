88141610c39880042bc6e0b2a9d2c6bfe477a78c
andres <andres@andress-MacBook-Pro.local>
2021-04-11 18:37:38 -0600

    log in UI demo looking good

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 // import TicketsBoard from './javascript/components/tickets_components/TicketsBoard'
-import Workspace from './javascript/components/workspace/Workspace'
+// import Workspace from './javascript/components/workspace/Workspace'
+import Home from './javascript/components/home/Home'
 function App() {
   return (
-    <Workspace></Workspace>
+    // <Workspace></Workspace>
+    <Home></Home>
   );
 }
 
